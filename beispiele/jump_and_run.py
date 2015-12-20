8aeaccd9e4ae966f7d8cd7eb92a355ab2824a297
Mark Weinreuter <mark@weinreuter.org>
2015-12-20 17:25:47 +0100

    auch mit 2 spielern

--- beispiele/jump_and_run.py
+++ beispiele/jump_and_run.py
@@ -3,12 +3,11 @@ from py2cd import ROT, BBox
 
 __author__ = 'Mark Weinreuter'
 
-# Initialisiert das Fenster
 Spiel.init(400, 400, "Jump and Run")
 
 
 class Figur(Bild):
-    def __init__(self, x, y, bild):
+    def __init__(self, x, y, bild, taste_links=T_LINKS, taste_rechts=T_RECHTS, taste_sprung=T_OBEN):
         super().__init__(x, y, bild)
         self.lauf_kraft = 3
         self.sprung_kraft = 10
@@ -16,7 +15,9 @@ class Figur(Bild):
         self.kann_kollidieren = []
         self.spruenge = 0
 
-        self.registriere_tasten(T_LINKS, T_RECHTS, T_OBEN)
+        Spiel.registriere_solange_taste_unten(taste_links, lambda dt: self.bewege_links_rechts(self.lauf_kraft * -dt))
+        Spiel.registriere_solange_taste_unten(taste_rechts, lambda dt: self.bewege_links_rechts(self.lauf_kraft * dt))
+        Spiel.registriere_taste_gedrueckt(taste_sprung, lambda unten, pyg_ereignis: self.springen(unten))
 
     def aktualisiere(self, dt):
         self.y_geschwindigkeit += self.gravitation
@@ -35,11 +36,6 @@ class Figur(Bild):
 
         self.aendere_position(0, bewegung_y)
 
-    def registriere_tasten(self, taste_links, taste_rechts, taste_sprung):
-        Spiel.registriere_solange_taste_unten(taste_links, lambda dt: self.bewege_links_rechts(self.lauf_kraft * -dt))
-        Spiel.registriere_solange_taste_unten(taste_rechts, lambda dt: self.bewege_links_rechts(self.lauf_kraft * dt))
-        Spiel.registriere_taste_gedrueckt(taste_sprung, lambda unten, pyg_ereignis: self.springen(unten))
-
     def springen(self, unten):
         if unten and self.spruenge < 2:
             self.spruenge += 1
@@ -57,9 +53,7 @@ class Figur(Bild):
 
 
 # Bild laden in den Speicher laden und unter dem Schlüssel "scratch" ablegen
-BildSpeicher.lade_bild("scratch", "testimages/wobbel.png")
-# Bild aus dem Speicher über seinen Schlüssel holen
-figur = Figur(10, 10, "scratch")
+BildSpeicher.lade_bild("wobbel", "testimages/wobbel.png")
 
 boden = Rechteck(0, 0, Spiel.breite, 20, ROT)
 boden.unten = 20
@@ -69,11 +63,16 @@ boden3 = Rechteck(350, 100, 40, 20, ROT)
 boden4 = Rechteck(270, 200, 40, 20, ROT)
 
 bloecke = [boden, boden2, boden3, boden4]
-figur.kann_kollidieren = bloecke
+
+figur1 = Figur(10, 10, "wobbel")
+figur1.kann_kollidieren = bloecke
+figur2 = Figur(310, 10, "wobbel", taste_links=T_a, taste_rechts=T_d, taste_sprung=T_w)
+figur2.kann_kollidieren = bloecke
 
 
 def aktualisiere(dt):
-    figur.aktualisiere(dt)
+    figur1.aktualisiere(dt)
+    figur2.aktualisiere(dt)
 
 
 # Das Spiel starten
